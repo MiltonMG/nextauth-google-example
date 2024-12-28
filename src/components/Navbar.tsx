978b37cc5da3e52a7d712374db324f0c212dfcd2
Milton MG <milton.elsalvador26@gmail.com>
2024-12-28 08:29:25 -0600

    second commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,42 @@
+"use client"
+import Link from "next/link"
+import { signIn, useSession, signOut } from "next-auth/react";
+
+const Navbar = () => {
+
+  const { data: session } = useSession();
+  //console.log(session);
+
+  return (
+    <nav className="bg-slate-900 text-white p-4 flex justify-between items-center px-24">
+      <Link href="/">
+        <h1>
+          Next Google
+        </h1>
+      </Link>
+      {session?.user ? (
+        <div className="flex items-center gap-x-2">
+          <Link href="/dashboard">
+            Dashboard
+          </Link>
+          <p className="cursor-pointer">
+            {session.user.name} ({session.user.email})
+          </p>
+          <img src={session.user.image} alt={session.user.name} className="h-8 w-8 rounded-full cursor-pointer" />
+          <button
+            onClick={() => signOut()}
+          >
+            LogOut
+          </button>
+        </div>
+      ) : (
+        <button onClick={() => signIn("google")} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Sign In
+        </button>
+      )}
+    </nav>
+
+  )
+}
+
+export default Navbar
